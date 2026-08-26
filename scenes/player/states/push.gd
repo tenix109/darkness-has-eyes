e9ea556020d357da81f9caf67f6654a45259484c
amuntean1127 <tenix109@gmail.com>
2026-08-26 01:50:09 -0400

    footstep sound and animation scale to push state

--- scenes/player/states/push.gd
+++ scenes/player/states/push.gd
@@ -1,12 +1,16 @@
 extends State
 
+@export var base_footstep_interval: float = 0.5
+var footstep_timer: float = 0.0
+
 @onready var player: Player = get_parent().get_parent() as Player
+
 
 func enter(_prev):
   player.set_facing_from_input(-player.pushable_grab_side)
-  actor.play_directional_anim("idle")
+  player.play_directional_anim("idle")
 
-func physics_update(_delta):
+func physics_update(delta):
   if player.is_dead or player.current_pushable == null:
     return
 
@@ -17,18 +21,29 @@ func physics_update(_delta):
   var along: float = input_vec.dot(axis)
   var axis_input: Vector2 = axis * along
 
-  var move_speed: float = actor.base_speed * actor.push_speed_modifier
-
+  var move_speed: float = player.base_speed * player.push_speed_modifier
   if axis_input.is_zero_approx():
-    actor.velocity = Vector2.ZERO
-    actor.play_directional_anim("idle")
-  else:
-    actor.velocity = axis_input.normalized() * move_speed
-    actor.play_directional_anim("walk")
+    player.velocity = Vector2.ZERO
+    player.play_directional_anim("idle")
+    player.animated_sprite.speed_scale = 1.0
+    footstep_timer = 0.0
+  else: # moving
+    player.velocity = axis_input.normalized() * move_speed
+    player.animated_sprite.speed_scale = player.push_speed_modifier
+    player.play_directional_anim("walk")
+    footstep_timer -= delta
+    if footstep_timer <= 0.0:
+      player.play_footstep()
+      footstep_timer = get_footstep_interval(move_speed)
 
   player.set_facing_from_input(-player.pushable_grab_side)
 
-  actor.global_position = pushable.global_position + actor.pushable_grab_offset
+  player.global_position = pushable.global_position + player.pushable_grab_offset
 
 func exit(_next):
-  actor.animated_sprite.speed_scale = 1.0
+  footstep_timer = 0.0
+  player.animated_sprite.speed_scale = 1.0
+
+func get_footstep_interval(current_speed: float) -> float:
+  var normalized: float = current_speed / player.base_speed
+  return clamp(base_footstep_interval / normalized, 0.15, 1.4)
